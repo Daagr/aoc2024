da07ab472df766f4dec49ab773532e8a2aad9a40
Mikael Ilmanen <daagr.i@gmail.com>
2024-12-03 00:13:13 +0200

    Day 2 Part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,10 +1,8 @@
 use aoc2024::input_file;
 use std::{error::Error, io::BufRead};
 
-fn is_safe(line: &str) -> bool {
-    let mut nums = line
-        .split_ascii_whitespace()
-        .map(|n| n.parse::<i32>().unwrap());
+fn is_safe(nums: &[i32]) -> bool {
+    let mut nums = nums.iter();
     let mut last = nums.next().unwrap();
     let mut increasing = None;
     for n in nums {
@@ -33,11 +31,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let filename = input_file(false);
     let lines = std::io::BufReader::new(std::fs::File::open(filename)?).lines();
     let mut count = 0;
+    let mut dampextra = 0;
     for line in lines {
-        if is_safe(&line?) {
+        let nums: Vec<i32> = line?
+            .split_ascii_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+        let safe = is_safe(&nums);
+        if safe {
             count += 1;
+        } else {
+            for i in 0..nums.len() {
+                let mut dnums = nums.clone();
+                dnums.remove(i);
+                if is_safe(&dnums) {
+                    dampextra += 1;
+                    break;
+                }
+            }
         }
     }
     println!("Safe count: {}", count);
+    println!("Damp safe: {}", count + dampextra);
     Ok(())
 }
