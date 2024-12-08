8fb43d0fb2c8ebb126507b949cb53b415781769b
Mikael Ilmanen <daagr.i@gmail.com>
2024-12-08 08:29:18 +0200

    Move Combs from day7 to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,65 @@ mod tests {
         assert_eq!(concat_nums(6, 10), 610);
     }
 }
+
+#[derive(Debug)]
+pub struct Combs<'a, Elem> {
+    len: usize,
+    n: usize,
+    elems: &'a [Elem],
+}
+impl<'a, Elem: Copy> Iterator for Combs<'a, Elem> {
+    type Item = Vec<Elem>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.n >= self.elems.len().pow(self.len as u32) {
+            return None;
+        }
+        let mut it = Vec::with_capacity(self.len);
+        let mut n = self.n;
+        for _ in 0..self.len {
+            it.push(self.elems[n % self.elems.len()]);
+            n /= self.elems.len();
+        }
+        self.n += 1;
+        Some(it)
+    }
+}
+impl<'a, Elem> Combs<'a, Elem> {
+    pub fn new(len: usize, elems: &'a [Elem]) -> Self {
+        Combs { len, n: 0, elems }
+    }
+}
+#[cfg(test)]
+mod tests_combs {
+    use super::*;
+    #[test]
+    fn test_combs() {
+        assert_eq!(
+            Combs::new(1, &[1, 2]).collect::<Vec<_>>(),
+            vec![vec![1], vec![2]]
+        );
+        assert_eq!(
+            Combs::new(2, &[1, 2]).collect::<Vec<_>>(),
+            vec![vec![1, 1], vec![2, 1], vec![1, 2], vec![2, 2]]
+        );
+        assert_eq!(
+            Combs::new(1, &[1, 2, 3]).collect::<Vec<_>>(),
+            vec![vec![1], vec![2], vec![3]]
+        );
+        assert_eq!(
+            Combs::new(2, &[1, 2, 3]).collect::<Vec<_>>(),
+            vec![
+                vec![1, 1],
+                vec![2, 1],
+                vec![3, 1],
+                vec![1, 2],
+                vec![2, 2],
+                vec![3, 2],
+                vec![1, 3],
+                vec![2, 3],
+                vec![3, 3]
+            ]
+        );
+    }
+}
